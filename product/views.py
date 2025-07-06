72f4a8cb2616a0fac637e1581cb80e802a449b4d
Aisultan <ajsultankockorbajuuluajsultan@gmail.com>
2025-07-07 00:04:27 +0600

    hw_1

---
+++ product/views.py
@@ -0,0 +1,72 @@
+from itertools import product
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product, Category, Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer, ProductDetailSerializer
+
+
+@api_view(['GET'])
+def category_list_api_view(request):
+    category = Category.objects.all()
+
+    data = CategorySerializer(instance=category, many=True).data
+
+    return Response(
+        data=data,
+        status=status.HTTP_200_OK
+    )
+
+@api_view(['GET'])
+def category_detail_api_view(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        return Response(data={'error': f'Category with id = {str(id)} does not exist!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = CategorySerializer(category).data
+    return Response(data=data)
+
+@api_view(http_method_names=['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+
+    data = ProductSerializer(instance=products, many=True).data
+
+    return Response(
+        data=data,
+        status=status.HTTP_200_OK
+    )
+
+@api_view(['GET'])
+def product_detail_api_view(request, id):
+    try:
+        products = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response(data={'error': f'Product with id = {str(id)} does not exist!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = ProductDetailSerializer(products).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    review = Review.objects.all()
+
+    data = ReviewSerializer(review, many=True).data
+    return Response(
+        data=data,
+        status=status.HTTP_200_OK
+    )
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Review.object.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error': f'Review with id = {str(id)} does not exist!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(review).data
+    return Response(data=data)
